7f7d2eb730776eb702dfaf28ed67d397899e3165
bedirhaneren <163941707+bedirhaneren@users.noreply.github.com>
2025-05-15 12:41:55 +0300

    Creating tables if they not exist

--- main.py
+++ main.py
@@ -1,61 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base , Kullanici, Hasta_doktor, Olcum, Insulin# tüm sınıflar Baseye bagli
-from datetime import datetime, date
-from sqlalchemy import func
+from models import create_tables
+from db_manager import DatabaseManager
+from datetime import date
 
+def main():
+    print("Veritabanı tabloları kontrol ediliyor...")
+    create_tables()
+    
+    print("Veritabanı tabloları başarıyla oluşturuldu.")
+    print("Program sonlandırılıyor...")
 
-engine = create_engine("postgresql+psycopg2://postgres:1234@localhost/glucosedb")
-
-Base.metadata.create_all(engine) #tablolar olsuturuyor zaten varsa atliyor.
-
-Session= sessionmaker(bind=engine)
-session = Session()
-
-# Hastayı çek
-hasta = session.query(Kullanici).filter_by(tc_kimlik_no="22222222222").first()
-
-# Bugüne ait ölçümleri al (ortalama dahil olanlar)
-bugun = date.today()
-
-olcumler = session.query(Olcum).filter(
-    Olcum.hasta_id == hasta.id,
-    func.date(Olcum.tarih_saat) == bugun,
-    Olcum.ortalamaya_dahil == True
-).all()
-
-# Ortalama hesapla
-if olcumler:
-    toplam = sum([o.kan_seker_degeri for o in olcumler])
-    adet = len(olcumler)
-    ortalama = toplam / adet
-else:
-    ortalama = None
-
-# Doz belirle
-doz = 0
-if ortalama is not None:
-    if ortalama < 70:
-        doz = 0
-    elif ortalama <= 110:
-        doz = 0
-    elif ortalama <= 150:
-        doz = 1
-    elif ortalama <= 200:
-        doz = 2
-    else:
-        doz = 3
-
-    # Kaydet
-    insulin_kaydi = Insulin(
-        hasta_id=hasta.id,
-        tarih=bugun,
-        ortalama_seker=ortalama,
-        doz_miktari=doz
-    )
-    session.add(insulin_kaydi)
-    session.commit()
-
-    print(f"✅ Ortalama: {ortalama:.2f} mg/dL → Önerilen doz: {doz} ml")
-else:
-    print("⚠️ Bugüne ait ölçüm verisi bulunamadı.")
+if __name__ == "__main__":
+    main()
